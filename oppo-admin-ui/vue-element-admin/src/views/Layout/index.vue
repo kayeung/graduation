e5b3c993d34b658e43d94879bc0913f56813bc24
kayeung <791047665@qq.com>
2022-03-01 23:05:58 +0800

    upload后台页面，写到layout两边分栏，做好css

---
+++ oppo-admin-ui/vue-element-admin/src/views/Layout/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="layout">
+    <!-- 左侧 -->
+    <Menu class="menu" />
+    <!-- 右侧 -->
+    <Content class="content" />
+  </div>
+</template>
+
+<script>
+import Menu from "./Menu.vue";
+import Content from "./content.vue";
+export default {
+  components: {
+    Menu,
+    Content,
+  },
+};
+</script>
+
+<style lang='less' scoped>
+.layout {
+  display: flex;
+  .menu {
+    width: 200px;
+    min-height: 500px;
+    background: rgb(48, 65, 86);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+  }
+  .content {
+    margin-left: 200px;
+  }
+}
+</style>
