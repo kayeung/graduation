bc78c1e69650339dc7b72728f09715b92768f6d3
kayeung <791047665@qq.com>
2022-03-03 19:56:15 +0800

    新项目加上代理跨域请求的代码

--- oppo-admin/vue-element-admin/vue.config.js
+++ oppo-admin/vue-element-admin/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    proxy: {
+      'api': {     //这里是公共部分，在调用接口时后面接不相同的部分，
+        target: 'http://localhost:9090/', //这里写的是访问接口的域名和端口号
+        changeOrigin: true, // 必须加上这个才能跨域请求
+        pathRewrite: {  // 重命名
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
