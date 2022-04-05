cca0044273146ffd5ba8925ca04abad11f6a5eb8
kayeung <791047665@qq.com>
2022-04-06 00:22:42 +0800

    门户网站移植至Vue

---
+++ oppo-web-ui/oppo-web-vue/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import $ from 'jquery'
+import './assets/css/bootstrap.min.css'
+import './assets/js/bootstrap.min'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
