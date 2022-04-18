1a314b33e70025c2f51e89fe78eb89863048f003
KY-DESKTOP\KY-Desktop <kayeung2020@gmail.com>
2022-04-19 00:18:14 +0800

    备注vuex内各字段的作用

--- oppo-admin/vue-element-admin/src/store/index.js
+++ oppo-admin/vue-element-admin/src/store/index.js
@@ -5,12 +5,16 @@ import loginModule from './modules/loginModule'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 全局state对象，用于保存所有组件的公共数据
   state: {
   },
+  // 监听state对象的值的最新状态（计算属性）
   getters: {
   },
+  // 唯一一个可以修改state值的方法（同步执行）
   mutations: {
   },
+  // 异步执行mutations方法
   actions: {
   },
   modules: {
