057c8502093542d212a7184350e3131ef352a62f
“Kayeung” <kayeung2020@gmail.com>
2022-05-05 09:29:51 +0800

    门户页脚适配手机端

--- oppo-web-ui/oppo-web-vue/src/components/footer.vue
+++ oppo-web-ui/oppo-web-vue/src/components/footer.vue
@@ -1,5 +1,4 @@
 <template>
-<!-- bug:目前电脑端footer已适配接口，手机端尚未适配 -->
   <div>
     <footer class="footer">
       <div class="container">
@@ -28,199 +27,40 @@
           role="tablist"
           aria-multiselectable="true"
         >
-          <div class="panel">
-            <div class="panel-heading" role="tab" id="headingOne">
+          <div
+            class="panel"
+            v-for="(item, index) in footerTable"
+            :key="item.id"
+          >
+            <div class="panel-heading" role="tab" :id="'heading' + index">
               <h4 class="panel-title">
                 <a
+                  :class="index != 0 ? collapsed : ''"
                   role="button"
                   data-toggle="collapse"
                   data-parent="#accordion"
-                  href="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
+                  :href="'#collapse' + index"
+                  :aria-expanded="index != 0 ? false : true"
+                  :aria-controls="'collapse' + index"
                 >
-                  推荐产品
-                  <!-- <span class="glyphicon glyphicon-plus oppo-tablist-icon"></span> -->
+                  {{ item.titleName }}
                 </a>
               </h4>
             </div>
             <div
-              id="collapseOne"
-              class="panel-collapse collapse in"
-              role="tabpanel"
-              aria-labelledby="headingOne"
-            >
-              <div class="panel-body">
-                <dl>
-                  <dd><router-link to="/">Find N</router-link></dd>
-                  <dd><router-link to="/">Find X3 系列</router-link></dd>
-                  <dd><router-link to="/">Reno7 系列</router-link></dd>
-                  <dd><router-link to="/">A96</router-link></dd>
-                  <dd><router-link to="/">K9x</router-link></dd>
-                  <dd><router-link to="/">Watch Free</router-link></dd>
-                  <dd><router-link to="/">查看全部手机</router-link></dd>
-                </dl>
-              </div>
-            </div>
-          </div>
-          <div class="panel">
-            <div class="panel-heading" role="tab" id="headingTwo">
-              <h4 class="panel-title">
-                <a
-                  class="collapsed"
-                  role="button"
-                  data-toggle="collapse"
-                  data-parent="#accordion"
-                  href="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
-                >
-                  购买渠道
-                </a>
-              </h4>
-            </div>
-            <div
-              id="collapseTwo"
+              :id="'collapse' + index"
               class="panel-collapse collapse"
+              :class="index != 0 ? '' : 'in'"
               role="tabpanel"
-              aria-labelledby="headingTwo"
+              :aria-labelledby="'heading' + index"
             >
               <div class="panel-body">
                 <dl>
-                  <dd><router-link to="/">线下体验店</router-link></dd>
-                  <dd><router-link to="/">OPPO商城</router-link></dd>
-                  <dd><router-link to="/">官方授权网点</router-link></dd>
-                </dl>
-              </div>
-            </div>
-          </div>
-          <div class="panel">
-            <div class="panel-heading" role="tab" id="headingThree">
-              <h4 class="panel-title">
-                <a
-                  class="collapsed"
-                  role="button"
-                  data-toggle="collapse"
-                  data-parent="#accordion"
-                  href="#collapseThree"
-                  aria-expanded="false"
-                  aria-controls="collapseThree"
-                >
-                  探索精彩OPPO
-                </a>
-              </h4>
-            </div>
-            <div
-              id="collapseThree"
-              class="panel-collapse collapse"
-              role="tabpanel"
-              aria-labelledby="headingThree"
-            >
-              <div class="panel-body">
-                <dl>
-                  <dd><router-link to="/">关于OPPO</router-link></dd>
-                  <dd><router-link to="/">新闻中心</router-link></dd>
-                  <dd><router-link to="/">OPPO社区</router-link></dd>
-                  <dd><router-link to="/">加入我们</router-link></dd>
-                  <dd><router-link to="/">知识产权</router-link></dd>
-                  <dd><router-link to="/">可持续发展报告</router-link></dd>
-                  <dd><router-link to="/">安全隐私</router-link></dd>
-                </dl>
-              </div>
-            </div>
-          </div>
-          <div class="panel">
-            <div class="panel-heading" role="tab" id="headingFour">
-              <h4 class="panel-title">
-                <a
-                  class="collapsed"
-                  role="button"
-                  data-toggle="collapse"
-                  data-parent="#accordion"
-                  href="#collapseFour"
-                  aria-expanded="false"
-                  aria-controls="collapseFour"
-                >
-                  商务合作
-                </a>
-              </h4>
-            </div>
-            <div
-              id="collapseFour"
-              class="panel-collapse collapse"
-              role="tabpanel"
-              aria-labelledby="headingFour"
-            >
-              <div class="panel-body">
-                <dl>
-                  <dd><router-link to="/">企业业务</router-link></dd>
-                  <dd><router-link to="/">开放平台</router-link></dd>
-                  <dd><router-link to="/">廉洁合规</router-link></dd>
-                </dl>
-              </div>
-            </div>
-          </div>
-          <div class="panel">
-            <div class="panel-heading" role="tab" id="headingFive">
-              <h4 class="panel-title">
-                <a
-                  class="collapsed"
-                  role="button"
-                  data-toggle="collapse"
-                  data-parent="#accordion"
-                  href="#collapseFive"
-                  aria-expanded="false"
-                  aria-controls="collapseFive"
-                >
-                  服务与支持
-                </a>
-              </h4>
-            </div>
-            <div
-              id="collapseFive"
-              class="panel-collapse collapse"
-              role="tabpanel"
-              aria-labelledby="headingFive"
-            >
-              <div class="panel-body">
-                <dl>
-                  <dd><router-link to="/">联系我们</router-link></dd>
-                  <dd><router-link to="/">服务网点查询</router-link></dd>
-                  <dd><router-link to="/">ColorOS</router-link></dd>
-                  <dd><router-link to="/">云服务</router-link></dd>
-                  <dd><router-link to="/">以旧换新</router-link></dd>
-                  <dd><router-link to="/">安全响应中心</router-link></dd>
-                </dl>
-              </div>
-            </div>
-          </div>
-          <div class="panel">
-            <div class="panel-heading" role="tab" id="headingSix">
-              <h4 class="panel-title">
-                <a
-                  class="collapsed"
-                  role="button"
-                  data-toggle="collapse"
-                  data-parent="#accordion"
-                  href="#collapseSix"
-                  aria-expanded="false"
-                  aria-controls="collapseSix"
-                >
-                  应用与下载
-                </a>
-              </h4>
-            </div>
-            <div
-              id="collapseSix"
-              class="panel-collapse collapse"
-              role="tabpanel"
-              aria-labelledby="headingSix"
-            >
-              <div class="panel-body">
-                <dl>
-                  <dd><router-link to="/">OPPO商城APP</router-link></dd>
-                  <dd><router-link to="/">预置软件公示</router-link></dd>
-                  <dd><router-link to="/">LOG专业调色文档</router-link></dd>
+                  <dd v-for="childItem in item.children" :key="childItem.id">
+                    <router-link :to="childItem.linkUrl">{{
+                      childItem.label
+                    }}</router-link>
+                  </dd>
                 </dl>
               </div>
             </div>
